da83a9e1dc0aa90ccdce2569f7e5f9b829bb84fb
marbisman <maria.arbisman@gmail.com>
2017-09-28 09:12:27 -0700

    Classwork Completed

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,8 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 	console.log("The DOM is ready!");
 
-	// squash the bugs isn't showing up
 	function addSubHeader() {
-		var subHeader = $('<small>&nbsp&nbspsquash the bugs!</small>')
+		var subHeader = $('<small>&nbsp&nbspsquash the bugs!</small>');
 		$('h1').append(subHeader);
 	}
 
@@ -17,16 +16,13 @@ $(document).ready(function(){
 		console.log('form submitted');
 		var input = $('input').eq(0).val();
 
-		// should count occurrences of the letter "E"
-		// and update the display. instead always shows 0
-		for (var i = 0; i < 10; i++){
-			// this loop doesn't find both es in  "supercalifragilisticexpialidocious"
-			var count = 0;
+		var count = 0;
+		for (var i = 0; i < input.length; i++){
 			if (input[i] === "e"){
 				count = count +1;
-			}
+			};
 			$('#num-es').text(count);
 		}
 	});
 
-};
+});
